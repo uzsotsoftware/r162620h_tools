684c0ca4cae42d2e2e88fe61e8be103e704626c5
R162620H <mutudzar@gmail.com>
2017-09-13 03:48:52 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -24,19 +24,19 @@ void setStrength(int  thisStr){
 strength=thisStr;
 }
  public:
-bool fight  (fight);
+bool fight  (bool fight);
 };
 class Rock : public Tool{
 public:
  Rock(int thisStr, char thisType){
 strength=thisStr;
-type=this ;
+type=thisType ;
 } 
 
 }; 
 class Paper:public Tool{
 public:
-void Paper(int thisStr,char thisType){
+ Paper(int thisStr,char thisType){
 strength=thisStr;
 }
 public:
@@ -45,7 +45,7 @@ bool fight(Tool fight);
 };
 class Scissors : public Tool{
 public:
-void Scissors(int thisStr, char thisType){
+ Scissors(int thisStr, char thisType){
 strength=thisStr;
 type=thisType;
 }
